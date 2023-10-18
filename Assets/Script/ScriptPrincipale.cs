6cddfe424fe8db46d7392895a21d2922aea77c70
victormichaud <victor.michaud.369@gmail.com>
2023-10-18 16:53:15 -0400

    Ajout du Script

---
+++ Assets/Script/ScriptPrincipale.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScriptPrincipale : MonoBehaviour
+{
+    
+
+
+    void Start()
+    {
+        Debug.Log("Début du Jeu");
+    }
+
+    void Update()
+    {
+        Debug.Log("La première ligne de code fonctionne - Victor");
+        Debug.Log("La deuxième ligne de code fonctionne - Victor");
+        Debug.Log("La troisième ligne de code fonctionne - Victor");
+    }
+}
